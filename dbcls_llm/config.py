7c2b74cf693d4e1d7cbe6c18f606ae2110caeb2c
Hirokazu Chiba <hchiba1@gmail.com>
2024-11-01 13:27:23 +0900

    Update config.py

--- dbcls_llm/config.py
+++ dbcls_llm/config.py
@@ -4,8 +4,8 @@ MODELS = {
     "gpt-35-turbo": [
         {"class": "gpt", "model": "gpt-35-turbo", "version": "0125", "tenant": "dbcls", "region": "northcentralus"},
         {"class": "gpt", "model": "gpt-35-turbo", "version": "1106", "tenant": "dbcls", "region": "australiaeast"},
-        {"class": "gpt", "model": "gpt-35-turbo", "version": "0613", "tenant": "chiba", "region": "australiaeast"},
-        {"class": "gpt", "model": "gpt-35-turbo", "version": "0301", "tenant": "chiba", "region": "southcentralus"}
+        {"class": "gpt", "model": "gpt-35-turbo", "version": "0613", "tenant": "dbcls", "region": "japaneast"},
+        {"class": "gpt", "model": "gpt-35-turbo", "version": "0301", "tenant": "dbcls", "region": "southcentralus"}
     ],
     "gpt-4o": [
         {"class": "gpt", "model": "gpt-4o", "version": "2024-05-13", "tenant": "dbcls", "region": "northcentralus"}
@@ -13,7 +13,7 @@ MODELS = {
     "gpt-4": [
         {"class": "gpt", "model": "gpt-4", "version": "0125-Preview", "tenant": "dbcls", "region": "northcentralus"},
         {"class": "gpt", "model": "gpt-4", "version": "1106-Preview", "tenant": "dbcls", "region": "australiaeast"},
-        {"class": "gpt", "model": "gpt-4", "version": "0613", "tenant": "chiba", "region": "australiaeast"}
+        {"class": "gpt", "model": "gpt-4-0613", "version": "0613", "tenant": "dbcls", "region": "australiaeast"}
     ],
     "claude3": [
         {"class": "claude3", "model": "opus"},
